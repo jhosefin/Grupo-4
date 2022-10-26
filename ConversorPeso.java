e2640f2b12eae47bab687801a9cd20f7672818c9
FelipeVidalV <felipe.vidval@gmail.com>
2022-10-26 10:11:31 -0300

    subo cambios con ArrayList

--- ConversorPeso.java
+++ ConversorPeso.java
@@ -1,4 +1,6 @@
 import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ConversorPeso {
 
@@ -7,27 +9,33 @@ public class ConversorPeso {
     public static void main(String[] args) {          
         Console console = System.console();          
         int veces = Integer.parseInt(console.readLine("Ingrese las veces que desea calcular su peso: "));
-              
+        ArrayList <Double> resultadosTransformacion = new ArrayList <Double> (Arrays.asList());
            
         while(veces!=0){
         
             int opcion = Integer.parseInt(console.readLine("Ingrese 1 : si desea covertir el peso en marte \n O Ingrese 2 : si desea covertir el peso en la luna \n"));
-                         
+            int peso1 = Integer.parseInt(console.readLine("Ingrese su peso: "));        
            if(opcion == 1){
-            int peso1 = Integer.parseInt(console.readLine("Ingrese su peso: ")); 
+             
             Double marte = (peso1 / 9.81 ) * 3.711 ; 
             System.out.println("El peso ingresado en Marte sería de: "+ marte); 
+            resultadosTransformacion.add(marte);
            }else if(opcion == 2){
-            int peso2 = Integer.parseInt(console.readLine("Ingrese su peso: "));
-            Double Luna =  peso2  /  9.81 * 1.622 ;             
-            System.out.println("El peso ingresado en La luna sería de: "+ Luna);  
 
+            
+            Double Luna =  peso1  /  9.81 * 1.622 ;             
+            System.out.println("El peso ingresado en La luna sería de: "+ Luna);  
+            resultadosTransformacion.add(Luna);
            }else{
             System.out.println("solo puede ingresar 1 o 2");
            }
            
                         
-                          
+          
+           
+           System.out.println(resultadosTransformacion);
+           
+
             
                         
                        
@@ -35,7 +43,7 @@ public class ConversorPeso {
            veces--;
     
         }
-    
+       
     }
 
      
